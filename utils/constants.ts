c84cb571c4b3fac8f907372154d6de2886462d3d
Mandeep Lali <mandeeplali@gmail.com>
2026-02-26 17:30:53 +0000

    add new rc code

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,4 @@
 export const MAX_HISTORY = 10;
 export const MAX_SEARCHES = 2;
-export const SUB_CHARGE = 1.49;
-export const GOOGLE_API_KEY_IN_RC = "goog_rGhDQaNWwutakYvgxRTXRMkWxPU";
+export const SUB_CHARGE = 2.00;
+export const GOOGLE_API_KEY_IN_RC = "goog_MQiNnbFxBJDBpMTsjhAAvMdyLQs";
