e4fce75c48f08327dfd2572d64c727a1a41177b3
Mandeep Lali <mandeeplali@gmail.com>
2026-03-07 08:08:01 +0000

    mv firebase config to env

--- utils/firebaseConfig.ts
+++ utils/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth, signInAnonymously } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKkQjb8wmZjWs37kbD202A6q-dk4inwog",
-  authDomain: "isitgood-54705.firebaseapp.com",
-  projectId: "isitgood-54705",
-  storageBucket: "isitgood-54705.firebasestorage.app",
-  messagingSenderId: "1026350183360",
-  appId: "1:1026350183360:android:a4c4bd90eaae66b2134e25"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,7 +18,6 @@ export const auth = getAuth(app);
 export const silentSignIn = async () => {
   try {
     if (auth.currentUser) return auth.currentUser.uid;
-
     const userCredential = await signInAnonymously(auth);
     return userCredential.user.uid;
   } catch (error) {
